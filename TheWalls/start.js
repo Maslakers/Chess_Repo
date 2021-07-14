51d93a138917ca4418e07eba52ba3b2b0fa289e5
Heppe <mateusz.chlopecki1@gmail.com>
2021-07-14 11:52:11 +0200

    murki - bugfixy i endscreen

--- TheWalls/start.js
+++ TheWalls/start.js
@@ -5,11 +5,13 @@ function start()
     if (!started)
     {
         started = true;
+        if(document.getElementById('opis') != null) 
         document.getElementById('board').removeChild(document.getElementById('opis'))
+
         let choice = document.createElement('div');
-        choice.innerHTML = "Wybierz Figurę:";
+        choice.innerHTML = "Wybierz Figurę:<br>";
         choice.id = 'choice';
-        document.body.appendChild(choice);
+        document.getElementById('panel').appendChild(choice);
         for(let i=0; i<4; i++)
         {
             let piece = document.createElement('img')
@@ -22,11 +24,58 @@ function start()
             document.getElementById("choice").appendChild(piece);
         }
     }
+    else
+    {
+        if(playing){
+            clearInterval(id);
+                let endScreen = document.createElement('div');
+                endScreen.id = 'opis';
+                endScreen.innerHTML = '<br><br><br><br><br>Koniec czasu! <br><br> Twój wynik: '+ points + 
+                ' p. <br><br> Wciśnij przycis Start! aby zagrać ponownie';
+                document.getElementById('board').appendChild(endScreen)
+                timeLeft = 0;
+                document.getElementById('timer').innerHTML = '1 : 00'
+                started = false;
+                playing = false;
+                start();
+        }
+        document.body.removeChild(document.getElementById('board'));
+        points = 0;
+        document.getElementById('pointCount').innerHTML = points;
+        let board = document.createElement('div');
+        board.id = 'board';
+        document.body.appendChild(board);
+        generateBoard();
+    }
 }
+let id=null;
 function play(piece)
 {
     if(!playing)
     {
+        document.getElementById('panel').removeChild(document.getElementById('choice'))
+        let timeLeft = 3;
+        id = setInterval(time, 1000)
+        function time()
+        {
+            if(timeLeft<=0)
+            {
+                clearInterval(id);
+                let endScreen = document.createElement('div');
+                endScreen.id = 'opis';
+                endScreen.innerHTML = '<br><br><br><br><br>Koniec czasu! <br><br> Twój wynik: '+ points + 
+                ' p. <br><br> Wciśnij przycis Start! aby zagrać ponownie';
+                document.getElementById('board').appendChild(endScreen)
+                timeLeft = 0;
+                playing = true;
+            }
+            else
+            {
+                timeLeft--;
+                document.getElementById('timer').innerHTML = '0 : '+ ((timeLeft < 10)? '0' + timeLeft : timeLeft);
+            }
+        }
+
         pieces[0] = new Piece('w', 4, 1, piece);
         playing = true;
         generateStuff()
@@ -38,19 +87,20 @@ function generateStuff()
 {
     let x = Math.floor(Math.random()*8)+1;
     let y = Math.floor(Math.random()*8)+1;
-    console.log(pieces[0].type, document.getElementById(x*10+y).className)
-    while(document.getElementById(x*10+y).firstChild != null || (pieces[0].type == 'B' 
+    while(document.getElementById(x*10+y).firstChild != null ||
+    pieces[0].x == x && pieces[0].y == y || 
+    (pieces[0].type == 'B' 
     && document.getElementById(x*10+y).className == 'darkSquare'))
     {
-        console.log(document.getElementById(x*10+y).classList)
         x = Math.floor(Math.random()*8)+1;
         y = Math.floor(Math.random()*8)+1;
     }
     pieces[1] = new Piece('b', x, y, 'p');
     x = Math.floor(Math.random()*8)+1;
     y = Math.floor(Math.random()*8)+1;
-    while(document.getElementById(x*10+y).firstChild != null || (pieces[0].type == 'B' 
-    && document.getElementById(x*10+y).className == 'darkSquare'))
+    while((pieces[1].x == x && pieces[1].y == y || pieces[0].x == x && pieces[0].y == y) || 
+    document.getElementById(x*10+y).firstChild != null ||
+    (pieces[0].type == 'B' && document.getElementById(x*10+y).className == 'darkSquare'))
     {
         x = Math.floor(Math.random()*8)+1;
         y = Math.floor(Math.random()*8)+1;
